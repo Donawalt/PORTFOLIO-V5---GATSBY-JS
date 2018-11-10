a6c65af7f7a71fe2ec468429b9751b9d25dfc4d2
Donaël Walt <w.dodo.a@hotmail.fr>
2018-11-10 18:30:50 +0100

    c

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import Footer from './footer'
 import Header from './header'
 import Helmet from 'react-helmet'
-import '../style/index.scss/'
+import '../style/index.scss'
 export default ({ children }) => <div className="app-wrapper">
 <Header/>
     {children}
