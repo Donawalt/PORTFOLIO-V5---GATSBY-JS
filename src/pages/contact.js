be9c13cbcf6c10f72ecc3a74a3f611494f1c8058
Donaël Walt <w.dodo.a@hotmail.fr>
2018-11-18 12:30:25 +0100

    contact net

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -22,7 +22,11 @@ export default () => (
         <p>Vous êtes intéressé pour que l'on travaille ensemble ou si vous cherchez juste une information.<br></br>Remplissez le formulaire ci-dessous!</p>
       </div>
       <div>
-      <form name="contact" action="https://formspree.io/donael.walter@hotmail.com" method="POST">
+      <form
+        name="contact"
+        methodx="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field">
           <label for="name">Nom:</label>
           <input type="text" name="name" placeholder="John Doe"/>
           <label for="email">Email:</label>
