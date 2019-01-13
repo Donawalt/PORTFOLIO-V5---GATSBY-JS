e644eb9fdde3557441ead4bf3a1fa538e13747bf
Donaël Walt <w.dodo.a@hotmail.fr>
2019-01-13 03:20:51 +0100

    change 404
    bkjbk

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 const NotFoundPage = () => (
-  <div id="c">
+  <div id="b">
     <Layout>
     <section>
     <h1>Error:<br/>'404'</h1>
