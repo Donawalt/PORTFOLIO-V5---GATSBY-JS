97f16c0ac745b276800dc121f13c5139c31802a9
Donaël Walt <w.dodo.a@hotmail.fr>
2019-01-13 13:39:27 +0100

    fix lang

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ export default ({ children }) => <div className="app-wrapper">
 <link href="https://fonts.googleapis.com/css?family=Roboto:700" rel="stylesheet"></link>
 <link href="https://fonts.googleapis.com/css?family=Montserrat:700,800" rel="stylesheet"></link>
 <meta name="description" content="Je suis étudiant en DUT MMI , futur Designer. Visitez mon site pour en connaître plus sur moi !"></meta>
-<meta name="language" content="EN"></meta>
+<meta name="language" content="FR"></meta>
 <link rel="alternate" href="https://en.donaelwalter.com/" hreflang="en-uk" />
 <link rel="alternate" href="https://en.donaelwalter.com/" hreflang="en-us" />
 </Helmet>
