2548372a528fbd8b1a54608577ee570c0ed0bd03
Donaël Walt <w.dodo.a@hotmail.fr>
2019-01-13 03:11:29 +0100

    404 pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,11 @@ import Layout from '../components/layout'
 const NotFoundPage = () => (
   <div id="c">
     <Layout>
+    <section>
+    <h1>Error:<br/>'404'</h1>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </section>
     </Layout>
   </div>
 )
