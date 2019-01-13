e2cd56a465c5425e466f216e35e465810a8ede37
Donaël Walt <w.dodo.a@hotmail.fr>
2019-01-13 13:26:36 +0100

    Language set

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,9 @@ export default ({ children }) => <div className="app-wrapper">
 <link href="https://fonts.googleapis.com/css?family=Roboto:700" rel="stylesheet"></link>
 <link href="https://fonts.googleapis.com/css?family=Montserrat:700,800" rel="stylesheet"></link>
 <meta name="description" content="Je suis étudiant en DUT MMI , futur Designer. Visitez mon site pour en connaître plus sur moi !"></meta>
+<meta name="language" content="EN"></meta>
+<link rel="alternate" href="https://en.donaelwalter.com/" hreflang="en-uk" />
+<link rel="alternate" href="https://en.donaelwalter.com/" hreflang="en-us" />
 </Helmet>
 <Header/>
     {children}
