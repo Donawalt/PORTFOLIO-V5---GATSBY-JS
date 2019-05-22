670bcf8adb4936272e0f8863485edcecd2b0afe4
Donaël Walter <w.dodo.a@hotmail.fr>
2019-05-22 16:16:49 +0200

    perfectionnement portfolio 22/05/2019

--- src/pages/a-propos.js
+++ src/pages/a-propos.js
@@ -1,29 +1,47 @@
 import React from 'react'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
-import CoverImg from '../assets/cover.jpg'
 
 export default () => (
   <div id="about">
     <Layout>
       <Helmet>
-        <title>A PROPOS || Donaël Walter</title>
+        <title>À PROPOS || Donaël Walter</title>
       </Helmet>
       <section id="c">
-        <h1>A-PROPOS</h1>
+        <h1>À-PROPOS</h1>
+        <div className="Couverture">
+          <img src="https://media.licdn.com/dms/image/C4D16AQGwXsTbc6iNIg/profile-displaybackgroundimage-shrink_350_1400/0?e=1564012800&v=beta&t=-WKGToBH0sKXUkWno9lkb7ucjlFW5wQy3w-adYcXlLM" />
+        </div>
         <p>
-          Née en 1999.<br />J'ai grandi dans les alentours de Valence en France.
+          Née en 1999.
+          <br />
+          J'ai grandi dans les alentours de Valence en France.
           <br />
           J'étudie le développement web et la communication visuelle à
-          l'Université de Dijon.<br />
+          l'Université de Dijon.
+          <br />
           Je souhaite devenir un développeur web créatif et un webdesigner
-          capable d'effectuer diverses tâches et suivre un projet à 360°.<br />
-          C'est pourquoi j'étudie en DUT MMI, pour dévélopper mes compétences en
-          graphisme, photographie, webdesign, et développement web.<br />
-          Toujours ouvert à de nouvelles opportunités, je recherche actuellement
-          un stage !
+          capable d'effectuer diverses tâches et suivre un projet à 360°.
+          <br />
+          Je suis actuellement en recherche d'un contrat de professionnalisation
+          dans le domaine du Webdesign et/ou du Développement Web, afin de
+          poursuivre mes études au sein de l'école{' '}
+          <a
+            href="https://www.hetic.net/formations/bachelor-web/programme-bachelor-web"
+            target="_blanck"
+          >
+            @HETIC
+          </a>{' '}
+          en bachelor web.
+          <br />
+          Toujours ouvert à de nouvelles opportunités, je suis déja disponible
+          pour des{' '}
+          <a href="https://www.malt.fr/profile/donawalt" target="_blanck">
+            missions en freelance
+          </a>
+          .
         </p>
       </section>
     </Layout>
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,20 +2,20 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 export default () => (
-<div id="Splash-Menu">
-<ul>
-<li>
-<Link to="/">Accueil</Link>
-</li>
-<li>
-<Link to="/skills">Compétences</Link>
-</li>
-<li>
-<Link to="/a-propos">A propos</Link>
-</li>
-<li>
-<Link to="/contact">Contact</Link>
-</li>
-</ul>
-</div>
+  <div id="Splash-Menu">
+    <ul>
+      <li>
+        <Link to="/">Accueil</Link>
+      </li>
+      <li>
+        <Link to="/skills">Compétences</Link>
+      </li>
+      <li>
+        <Link to="/a-propos">À propos</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </ul>
+  </div>
 )
