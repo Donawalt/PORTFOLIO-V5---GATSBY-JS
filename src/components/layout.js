7726ab038e51b5133d92080e7c91f4cd4d520d7d
Donaël Walter <w.dodo.a@hotmail.fr>
2019-05-22 17:09:00 +0200

    description update 22/05/2019

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,32 @@
-import React from "react"
+import React from 'react'
 import Footer from './footer'
 import Header from './header'
 import Helmet from 'react-helmet'
 import '../style/index.scss'
-export default ({ children }) => <div className="app-wrapper">
-<Helmet>
-<link href="https://fonts.googleapis.com/css?family=Roboto:700" rel="stylesheet"></link>
-<link href="https://fonts.googleapis.com/css?family=Montserrat:700,800" rel="stylesheet"></link>
-<meta name="description" content="I am a student in MMI DUT , future Designer. Visit my site to learn more about me!"></meta>
-<meta name="language" content="EN"></meta>
-<link rel="alternate" href="https://www.donaelwalter.com/" hreflang="fr-fr" />
-</Helmet>
-<Header/>
+export default ({ children }) => (
+  <div className="app-wrapper">
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:700"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Montserrat:700,800"
+        rel="stylesheet"
+      />
+      <meta
+        name="description"
+        content="A la recherche d'un contrat de professionnalisation en Webdesign ou Développement Web. DUT MMI DIJON prochainement HETIC. Développeur Front-End Freelance."
+      />
+      <meta name="language" content="fr" />
+      <link
+        rel="alternate"
+        href="https://en.donaelwalter.com/"
+        hreflang="en-en"
+      />
+    </Helmet>
+    <Header />
     {children}
-<Footer/>
-</div>
+    <Footer />
+  </div>
+)
